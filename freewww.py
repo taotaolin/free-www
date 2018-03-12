266e04303bb2bfa70d289285126b978b1105141e
Ramik0 <taotaolin@outlook.com>
2018-03-12 23:38:19 +0800

    fix bug

--- freewww.py
+++ freewww.py
@@ -7,10 +7,10 @@ ID = '机器ID'
 check = 'vm'
 restart = 'vm.restart'
 
+#此处的机房请修改为自己机器所在的，目前所知kvm,kvm1,kvm2...
 checkurl = 'http://kvm.free-www.ru:1500/vmmgr?out=xml&authinfo={0}:{1}&func={2}&elid={3}'.format(username,password,check,ID)
 keepurl = 'http://kvm.free-www.ru:1500/vmmgr?out=xml&authinfo={0}:{1}&func={2}&elid={3}'.format(username,password,restart,ID)
 list1 = [('ok', 'vm.restart')]
-
 
 def check(url = checkurl):
 	try:
@@ -29,7 +29,7 @@ def restart(url = keepurl):
 		r.raise_for_status()
 		r.encoding = r.apparent_encoding
 		khtml = r.text
-		result = re.findall(r'><(.*)/><tparams><elid>237</elid><out>xml</out><func>(.*)</func>',khtml)
+		result = re.findall(r'><(.*)/><tparams><elid>%s</elid><out>xml</out><func>(.*)</func>'%ID,khtml)
 		if list1 == result:
 			print("Success Runing!")
 		else:
@@ -39,11 +39,16 @@ def restart(url = keepurl):
 
 def main():
 	rhtml = check()
+	id_num = re.findall(r'<id>(\d+)</id>',rhtml)
 	state = re.findall(r'<vmstatus>(.*)</vmstatus>',rhtml)
-	if "running" in state:
-		print("free-www is Running")
-	else:
-		restart()
+	if len(state) != 1:
+			print("检测到不止一台VPS")
+		if "running" in state[id_num.index(ID)]:
+			print("Free-www is Running")
+		else:
+			print("%s is not running"%ID)
+			restart()
+
 
 if __name__ == '__main__':
 	while True:
