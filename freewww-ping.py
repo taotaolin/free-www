ae54126e98cf8352a37c31148c1f9b98f935d89c
Ramik0 <taotaolin@outlook.com>
2018-03-12 23:31:43 +0800

    fix

--- freewww-ping.py
+++ freewww-ping.py
@@ -3,16 +3,15 @@ import requests,re,time,os
 
 username = '面板用户'
 password = '面板密码' 
-ID = '机器ID'
+ID = '检测机器ID'
 ip = '检测机器IP'
 check = 'vm'
 restart = 'vm.restart'
 
-
+#此处的机房请修改为自己机器所在的，目前所知kvm,kvm1,kvm2...
 checkurl = 'http://kvm.free-www.ru:1500/vmmgr?out=xml&authinfo={0}:{1}&func={2}&elid={3}'.format(username,password,check,ID)
 keepurl = 'http://kvm.free-www.ru:1500/vmmgr?out=xml&authinfo={0}:{1}&func={2}&elid={3}'.format(username,password,restart,ID)
 list1 = [('ok', 'vm.restart')]
-
 
 def check(url = checkurl):
 	try:
@@ -31,7 +30,7 @@ def restart(url = keepurl):
 		r.raise_for_status()
 		r.encoding = r.apparent_encoding
 		khtml = r.text
-		result = re.findall(r'><(.*)/><tparams><elid>237</elid><out>xml</out><func>(.*)</func>',khtml)
+		result = re.findall(r'><(.*)/><tparams><elid>%s</elid><out>xml</out><func>(.*)</func>'%ID,khtml)
 		if list1 == result:
 			print("Success Runing!")
 		else:
@@ -42,12 +41,16 @@ def restart(url = keepurl):
 def main():
 	if(os.system('ping -c 5 -w 5 %s'%ip) == 0):
 		print("Free-www is Running")
-	else:
+	else: 
 		rhtml = check()
+		id_num = re.findall(r'<id>(\d+)</id>',rhtml)
 		state = re.findall(r'<vmstatus>(.*)</vmstatus>',rhtml)
-		if "running" in state:
+		if len(state) != 1:
+			print("检测到不止一台VPS")
+		if "running" in state[id_num.index(ID)]:
 			print("Free-www is Running")
 		else:
+			print("%s is not running"%ID)
 			restart()
 
 if __name__ == '__main__':
