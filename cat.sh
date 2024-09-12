f1da3be851ef8d5949ba843c031a4f4c1793bd22
Ramik0 <taotaolin@outlook.com>
2024-09-12 14:42:12 +0800

    Update cat.sh

--- cat.sh
+++ cat.sh
@@ -1,54 +1,76 @@
 #!/bin/bash
 
-echo  "更新源"
-apt-get update
-apt-get install curl wget -y
-echo  "安装docker"
-apt-get install docker.io -y
-
+check_root
+echo  "upgrade"
+sudo apt update && sudo apt upgrade -y
+sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential git make ncdu unzip zip docker.io -y
+echo  "install docker"
 VERSION=$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | grep -Po '"tag_name": "\K.*\d')
-
 DESTINATION=/usr/local/bin/docker-compose
 curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
 chmod 755 $DESTINATION
-
-echo  "安装node"
+echo  "install node"
 apt-get install npm -y
 npm install n -g
 n stable
 npm i -g yarn
 
-
-echo  "拉取cat库"
+echo  "pull cat-git"
 git clone https://github.com/CATProtocol/cat-token-box
-
-
-echo  "安装cat库"
 cd cat-token-box
-yarn install && yarn build
+yarn install
+yarn build
 
-# 定义要写入的内容
-script_content='#!/bin/bash
+cd cat-token-box/packages/tracker/
+chmod 777 docker/data
+chmod 777 docker/pgdata
+docker-compose up -d
 
-command="sudo yarn cli mint -i 45ee725c2c5993b3e4d308842d87e973bf1951f5f7a804b21e4dd964ecd12d6b_0 5"
+cd ../../
+docker build -t tracker:latest .
+docker run -d \
+    --name tracker \
+    --add-host="host.docker.internal:host-gateway" \
+    -e DATABASE_HOST="host.docker.internal" \
+    -e RPC_HOST="host.docker.internal" \
+    -p 3000:3000 \
+    tracker:latest
 
+
+#创建快速重启tracker
+cat >"/root/cat-token-box/packages/cli/mint.sh"<<EOF
+#!/bin/bash
 while true; do
-    $command
-
-    if [ $? -ne 0 ]; then
-        echo "命令执行失败，退出循环"
-        exit 1
-    fi
-
-    sleep 15
+  gas=$(curl -s https://explorer.unisat.io/fractal-mainnet/api/bitcoin-info/fee | jq -r '.data.halfHourFee')
+  if [[ $gas -lt 300 ]]; then
+    sudo yarn cli mint -i 45ee725c2c5993b3e4d308842d87e973bf1951f5f7a804b21e4dd964ecd12d6b_0 5 --max-fee-rate=$gas
+  else
+    echo "当前gas：$gas,不执行，继续等待....."
+  fi
+  sleep 10
 done
-'
+EOF
+chmod +x /root/cat-token-box/packages/cli/mint.sh
 
-# 将内容写入到 s.sh 文件
-echo "$script_content" > s.sh
+#创建快速重启tracker
+cat >"/usr/local/bin/retracker"<<EOF
+#!/bin/bash
+dockerid=`docker ps -a | grep tracker:latest | awk '{print $1}'`
+docker restart $dockerid
+echo "重启tracker.."
+EOF
+chmod +x /usr/local/bin/retracker
 
-# 为 s.sh 添加执行权限
-chmod +x s.sh
+
+#创建快速查吨高度
+cat >"/usr/local/bin/getbl0ck"<<EOF
+#!/bin/bash
+curl 127.0.0.1:3000/api
+EOF
+chmod +x /usr/local/bin/getbl0ck
 
 
-sed -i 's/"maxFeeRate":.*/"maxFeeRate": 500,/' /root/cat-token-box/packages/cli/config.json
+
+
+
+
